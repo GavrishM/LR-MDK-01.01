1560c0f7818781772b72b9df968520720a13fd0e
Гавриш М <nanotechnology930@gmail.com>
2025-09-25 09:57:30 +0300

    Добавлены все нужные переменные и структуры данных

---
+++ ЛР1/Number2/Number2/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Number2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Random rnd = new Random();
+            int[] ints = new int[5000];
+            List<int> largeInts = new List<int>();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        }
+    }
+}
