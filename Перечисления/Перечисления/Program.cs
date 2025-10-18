c3d9a76123f9584eb0c30d2c05fecd14cd915a45
st50 <st50@WS50-15>
2025-10-18 10:50:38 +0300

    Добавлен вывод цвета

--- Перечисления/Перечисления/Program.cs
+++ Перечисления/Перечисления/Program.cs
@@ -40,13 +40,25 @@ namespace Перечисления
         }
         static void Main(string[] args)
         {
-            
+            Console.WriteLine("012");
+            int color = Convert.ToInt32(Console.ReadLine());
+            switch (color)
+            {
+                case (0):
+                    Console.ForegroundColor = ConsoleColor.Red;
+                    Console.WriteLine(Colors.Red);
+                    break;
+                case (1):
+                    Console.ForegroundColor = ConsoleColor.Green;
+                    Console.WriteLine(Colors.Green);
+                    break;
+                case (2):
+                    Console.ForegroundColor = ConsoleColor.Blue;
+                    Console.WriteLine(Colors.Blue);
+                    break;
 
-            
-        
-            
-
-
+            }
+            Console.ForegroundColor= ConsoleColor.White;
         }
     }
 }
