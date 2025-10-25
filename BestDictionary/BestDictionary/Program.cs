9c14c09213b93e323b7c844b566e342797e9acf2
st50 <st50@WS50-15>
2025-10-25 12:31:11 +0300

    Задача выполнена

--- BestDictionary/BestDictionary/Program.cs
+++ BestDictionary/BestDictionary/Program.cs
@@ -14,7 +14,7 @@ namespace BestDictionary
             //1. Создать произвольный ПУСТОЙ словарь, где ключ - строка, а значение список строк.
             //2. Добавить 3 ключа со своими значениями.
             //3. Вывести на экран все ключи словаря.
-            //4. Вывести на экран значение через ", " по ключу Х (любому).
+            //4. Вывести на экран значение через ", " по ключу X (любому).
             //5. 
             //6. 
             //7. 
@@ -22,10 +22,12 @@ namespace BestDictionary
 
             Dictionary<string, List<string>> Dict = new Dictionary<string, List<string>>();
             Dict.Add("colors", new List<string>() {"red", "green", "blue"});
-            Dict.Add("birds", new List<string>() { "crow", "pigeon", "parrot"});
-            Dict.Add("locations", new List<string>() { "orange_forest", "barier", "dark_valley"});
+            Dict.Add("birds", new List<string>() { "crow", "parrot"});
+            Dict.Add("locations", new List<string>() { "orange_forest"});
             string keys = string.Join(",", Dict.Keys);
             Console.WriteLine(keys);
+            string X = "colors";
+            Console.WriteLine(string.Join(",", Dict[X]));
         }
     }
 }
