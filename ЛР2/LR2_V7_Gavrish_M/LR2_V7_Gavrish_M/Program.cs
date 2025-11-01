5631f309f05bf2bff2242d369f0e8cdbc7bdf5ea
GavrishM <nanotechnology930@gmail.com>
2025-11-01 04:56:29 +0300

    Создана функция (ввод запроса пользователем)

--- ЛР2/LR2_V7_Gavrish_M/LR2_V7_Gavrish_M/Program.cs
+++ ЛР2/LR2_V7_Gavrish_M/LR2_V7_Gavrish_M/Program.cs
@@ -8,6 +8,25 @@ namespace LR2_V7_Gavrish_M
 {
     internal class Program
     {
+        static string inputUserQuery(Dictionary<string, Dictionary<string, List<int>>> categories)
+        {
+            string UserQuery = "";
+            bool chek = true;
+            while (chek)
+            {
+                Console.WriteLine("Введи название категории");
+                UserQuery = Console.ReadLine().ToLower();
+                if (categories.ContainsKey(UserQuery))
+                {
+                    chek = false;
+                }
+                else
+                {
+                    Console.WriteLine("Неправильно. Попробуй еще раз.");
+                }
+            }
+            return(UserQuery);
+        }
         static void Main(string[] args)
         {
           
