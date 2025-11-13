d48ea450aa0df80d943a089286f851b4f371263c
GavrishM <nanotechnology930@gmail.com>
2025-11-13 05:48:17 +0300

    Созданы структуры Day и Sale

---
+++ Дистанционное обучение/ВременнойРяд/ВременнойРяд/Day.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ВременнойРяд
+{
+    public struct Day
+    {
+        public DateTime Date;       //   Дата
+        public List<Sale> SaleList; //   Список продаж за день
+    }
+}
---
+++ Дистанционное обучение/ВременнойРяд/ВременнойРяд/Sale.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ВременнойРяд
+{
+    public struct Sale              //   Продажа телефона\телефонов
+    {                               //
+        public string PhoneModel;   //   Модель телефона
+        public int Quantity;        //   Количество
+        public int Price;           //   Цена
+                                    // V Цена купленных телефонов
+        static public int SalePrice (int Quantity, int Price)
+        {
+            int salePrice = Quantity * Price;
+            return salePrice;
+        }
+
+
+    }
+}
