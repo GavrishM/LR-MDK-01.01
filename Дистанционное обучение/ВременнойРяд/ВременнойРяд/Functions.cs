c8a18555ab4ad944182c034f7f356e9441b731a9
GavrishM <nanotechnology930@gmail.com>
2025-11-13 06:51:53 +0300

    Добавлена функция
    функция поиска суммы проданного за период х

---
+++ Дистанционное обучение/ВременнойРяд/ВременнойРяд/Functions.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ВременнойРяд
+{
+    internal class Functions
+    {
+        static public int FindSalesPrice(List<Day> days, DateTime start, DateTime end)
+        {
+            int salesPrice = 0;
+            foreach(Day day in days) 
+            {
+                if (day.Date > start | day.Date < end)
+                {
+                    foreach (Sale sale in day.SaleList)
+                    {
+                        salesPrice += Sale.SalePrice(sale.Quantity, sale.Price);
+                    }
+                } 
+            }
+            return salesPrice;
+        }
+
+
+
+
+
+    }
+}
