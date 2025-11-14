83e7ef88fe10e75a0419b4143592c34fda9316ef
Andrey-Shelgunov <shelgunova3@gmail.com>
2025-11-14 11:56:46 +0300

    Добавлен массив для генерации названий игр

--- DZ_08.11.25/Program.cs
+++ DZ_08.11.25/Program.cs
@@ -10,6 +10,60 @@ namespace DZ_08._11._25
     {
         static void Main(string[] args)
         {
+            string[,] gameNameRnd = new string[10, 4];
+            gameNameRnd[0, 0] = "";
+            gameNameRnd[1, 0] = "";
+            gameNameRnd[2, 0] = "";
+            gameNameRnd[3, 0] = "";
+            gameNameRnd[4, 0] = "";
+            gameNameRnd[5, 0] = "";
+            gameNameRnd[6, 0] = "";
+            gameNameRnd[7, 0] = "";
+            gameNameRnd[8, 0] = "";
+            gameNameRnd[9, 0] = "";
+            gameNameRnd[0, 1] = "";
+            gameNameRnd[1, 1] = "";
+            gameNameRnd[2, 1] = "";
+            gameNameRnd[3, 1] = "";
+            gameNameRnd[6, 1] = "";
+            gameNameRnd[7, 1] = "";
+            gameNameRnd[8, 1] = "";
+            gameNameRnd[4, 1] = "";
+            gameNameRnd[5, 1] = "";
+            gameNameRnd[9, 1] = "";
+            gameNameRnd[0, 2] = "";
+            gameNameRnd[1, 2] = "";
+            gameNameRnd[2, 2] = "";
+            gameNameRnd[3, 2] = "";
+            gameNameRnd[4, 2] = "";
+            gameNameRnd[5, 2] = "";
+            gameNameRnd[6, 2] = "";
+            gameNameRnd[7, 2] = "";
+            gameNameRnd[8, 2] = "";
+            gameNameRnd[9, 2] = "";
+            gameNameRnd[0, 3] = "";
+            gameNameRnd[1, 3] = "";
+            gameNameRnd[2, 3] = "";
+            gameNameRnd[3, 3] = "";
+            gameNameRnd[4, 3] = "";
+            gameNameRnd[5, 3] = "";
+            gameNameRnd[6, 3] = "";
+            gameNameRnd[7, 3] = "";
+            gameNameRnd[8, 3] = "";
+            gameNameRnd[9, 3] = "";
+
+
+
+
+
+
+
+
+
+
+
+
+
 
         }
     }
