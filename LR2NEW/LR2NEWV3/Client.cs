ff9b129cb037b31df796d5a82a6832b8c3815062
Andrey-Shelgunov <shelgunova3@gmail.com>
2025-11-15 11:52:20 +0300

    Созданы класс для функций и структуры услуг и клиентов

---
+++ LR2NEW/LR2NEWV3/Client.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LR2NEWV3
+{
+    public struct Client
+    {
+        public string name;
+        public List<Servise> servises;
+    }
+}
---
+++ LR2NEW/LR2NEWV3/Servise.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LR2NEWV3
+{
+    public struct Servise
+    {
+        public string name;
+    }
+}
