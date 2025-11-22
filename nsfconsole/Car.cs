3590b796a76219278584c90b7609959fa9eb722e
a.boikov <alex8oikov@yandex.ru>
2025-11-22 10:29:37 +0300

    Создан список объектов

---
+++ nsfconsole/Car.cs
@@ -0,0 +1,26 @@
+﻿
+
+namespace nsfconsole
+{
+    public class Car
+    {
+        private string model_;
+        private int speed_;
+
+        public void SetModel(string model)
+        {
+            model_ = model;
+        }
+        public string GetModel()
+        { 
+        return model_;
+        }
+        public void SetSpeed(int speed)
+        {
+            speed_ = speed;
+        }
+
+
+
+    }
+}
---
+++ nsfconsole/Program.cs
@@ -0,0 +1,34 @@
+﻿
+
+using System;
+using System.Collections.Generic;
+
+namespace nsfconsole
+{
+    internal class Program
+    {
+        static void PrintModel(Car car)
+        {
+            Console.WriteLine(car.GetModel());
+        }
+        static void Main()
+        {
+            
+            Car firstCar = new Car();
+            firstCar.SetModel("Lada");
+            firstCar.SetSpeed(150);
+            PrintModel(firstCar);
+            Car secondCar = new Car();
+            secondCar.SetModel("Merc");
+            secondCar.SetSpeed(300);
+            PrintModel(secondCar);
+
+            List<Car> cars = new List<Car>();
+            cars.Add(firstCar);
+            cars.Add(secondCar);
+
+
+
+        }
+    }
+}
