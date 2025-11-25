52cc5b90ba0e1c98a3d36a3d264cff986aa4c927
Andrey-Shelgunov <shelgunova3@gmail.com>
2025-11-25 12:35:31 +0300

    Создан класс Storage

---
+++ ManyClasses/ManyClasses/Storage.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ManyClasses
+{
+    public class Storage
+    {
+        private int Id_;
+        private string Address_;
+        private Dictionary<Product, double> Products_;
+
+
+
+
+
+
+
+
+
+    }
+}
