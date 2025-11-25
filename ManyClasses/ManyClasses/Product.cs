6436fc63f24dd5e1c6ef62755694c4422f246942
Andrey-Shelgunov <shelgunova3@gmail.com>
2025-11-25 12:23:02 +0300

    Создан класс Product

---
+++ ManyClasses/ManyClasses/Product.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ManyClasses
+{
+    public class Product
+    {
+        private string name_;
+        private double price_;
+
+
+    }
+}
