9001f3a9b27884b03ec02f6cf2c1dc2e7607e18d
GavrishM <nanotechnology930@gmail.com>
2025-12-02 11:06:46 +0300

    Созданы методы для задание и вывода полей класса Array

---
+++ Massive array/Massive array/Array.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Massive_array
+{
+    public class Array
+    {
+        private int Heigth_;
+        private int Width_;
+        private int[,] Array_;
+
+        public void SetHeigth(int heigth)
+        {
+            Heigth_ = heigth;
+        }
+        public void SetWidth(int width)
+        {
+            Width_ = width;
+        }
+        public void InitializeArray()
+        {
+            Array_ = new int[Width_, Heigth_];
+        }
+
+        public int GetHeigth()
+        {
+            return Heigth_;
+        }
+        public int GetWidth()
+        {
+            return Width_;
+        }
+
+        
+    }
+}
