0bcf9ad55ebe4fab8ac23dc472b031dd7d096ce8
GavrishM <st50@WS50-15>
2026-02-05 11:47:06 +0300

    Добавлены панели для размещения элементов

--- LR3/MainForm.Designer.cs
+++ LR3/MainForm.Designer.cs
@@ -28,20 +28,67 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.MainPanel = new System.Windows.Forms.Panel();
+            this.ListBoxPanel = new System.Windows.Forms.Panel();
+            this.ImageBoxPanel = new System.Windows.Forms.Panel();
+            this.TextBoxPanel = new System.Windows.Forms.Panel();
+            this.MainPanel.SuspendLayout();
             this.SuspendLayout();
+            // 
+            // MainPanel
+            // 
+            this.MainPanel.Controls.Add(this.TextBoxPanel);
+            this.MainPanel.Controls.Add(this.ImageBoxPanel);
+            this.MainPanel.Controls.Add(this.ListBoxPanel);
+            this.MainPanel.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.MainPanel.Location = new System.Drawing.Point(0, 0);
+            this.MainPanel.Name = "MainPanel";
+            this.MainPanel.Size = new System.Drawing.Size(800, 450);
+            this.MainPanel.TabIndex = 0;
+            // 
+            // ListBoxPanel
+            // 
+            this.ListBoxPanel.Dock = System.Windows.Forms.DockStyle.Left;
+            this.ListBoxPanel.Location = new System.Drawing.Point(0, 0);
+            this.ListBoxPanel.Name = "ListBoxPanel";
+            this.ListBoxPanel.Size = new System.Drawing.Size(166, 450);
+            this.ListBoxPanel.TabIndex = 0;
+            // 
+            // ImageBoxPanel
+            // 
+            this.ImageBoxPanel.Dock = System.Windows.Forms.DockStyle.Right;
+            this.ImageBoxPanel.Location = new System.Drawing.Point(365, 0);
+            this.ImageBoxPanel.Name = "ImageBoxPanel";
+            this.ImageBoxPanel.Size = new System.Drawing.Size(435, 450);
+            this.ImageBoxPanel.TabIndex = 1;
+            // 
+            // TextBoxPanel
+            // 
+            this.TextBoxPanel.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.TextBoxPanel.Location = new System.Drawing.Point(166, 0);
+            this.TextBoxPanel.Name = "TextBoxPanel";
+            this.TextBoxPanel.Size = new System.Drawing.Size(199, 450);
+            this.TextBoxPanel.TabIndex = 2;
             // 
             // MainForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.MainPanel);
             this.Name = "MainForm";
             this.Text = "Лекарства";
+            this.MainPanel.ResumeLayout(false);
             this.ResumeLayout(false);
 
         }
 
         #endregion
+
+        private System.Windows.Forms.Panel MainPanel;
+        private System.Windows.Forms.Panel ListBoxPanel;
+        private System.Windows.Forms.Panel TextBoxPanel;
+        private System.Windows.Forms.Panel ImageBoxPanel;
     }
 }
 
