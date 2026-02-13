62b7d18630f555a6885caa8785fd8468714124b5
GavrishM <nanotechnology930@gmail.com>
2026-02-13 08:44:04 +0300

    Добавлена библиотека классов

---
+++ WindowsFormsApp1/WindowsFormsApp1/Form1.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+    
+
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1: Form
+    {
+        int n,m,i,j;
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            foreach (DataGridViewRow row in dataGridView1.SelectedRows)
+            {
+                dataGridView1.Rows.Remove(row);
+            } 
+
+           
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            n = dataGridView1.RowCount;
+            m = dataGridView1.ColumnCount;
+            StreamWriter sw = File.CreateText("data.txt");
+            sw.WriteLine(Convert.ToString(n));
+            sw.WriteLine(Convert.ToString(m));
+            for (i=0; i<n; i++)
+            {
+                for (j=0;j<m;j++)
+                {
+                    sw.WriteLine(Convert.ToString(dataGridView1[j, i].Value));
+                }
+            }
+            sw.Close();
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            StreamReader f = new StreamReader("data.txt");
+            n = Convert.ToInt32(f.ReadLine());
+            m = Convert.ToInt32(f.ReadLine());
+            for (i=0;i<n;i++)
+            {
+                int rowNumber = dataGridView1.Rows.Add();
+                dataGridView1.Rows[rowNumber].Cells["ID"].Value = f.ReadLine();
+                dataGridView1.Rows[rowNumber].Cells[1].Value = f.ReadLine();
+                dataGridView1.Rows[rowNumber].Cells[2].Value = f.ReadLine();
+                dataGridView1.Rows[rowNumber].Cells["Tel"].Value = f.ReadLine();
+            }
+            f.Close();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            dataGridView1.Columns.Add("ID", "Id");
+            dataGridView1.Columns.Add("Fa", "Фамилия");
+            dataGridView1.Columns.Add("Name", "Имя");
+            dataGridView1.Columns.Add("Tel", "Телефон");
+        }
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            int rowNumber = dataGridView1.Rows.Add();
+            dataGridView1.Rows[rowNumber].Cells["ID"].Value = rowNumber;
+            dataGridView1.Rows[rowNumber].Cells[1].Value = textBox3.Text;
+            dataGridView1.Rows[rowNumber].Cells[2].Value = textBox2.Text;
+            dataGridView1.Rows[rowNumber].Cells["Tel"].Value = textBox1.Text;
+            textBox1.Text = " ";
+            textBox2.Text = " ";
+            textBox3.Text = " ";
+        }
+    }
+}
