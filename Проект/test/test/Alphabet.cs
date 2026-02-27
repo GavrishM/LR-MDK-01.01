fa0fdcd2cb7dd02a155cdcc1af8c9500d6682ad8
GavrishM <nanotechnology930@gmail.com>
2026-02-27 11:07:31 +0300

    Добавлен тестовый проект
    И проверена возможность превратить строку в char[]

---
+++ Проект/test/test/Alphabet.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace test
+{
+    public class Alphabet
+    {
+        public char[] Chars { get; set; }
+        public Alphabet(string language) 
+        {
+            if (language == "ru")
+            {
+                char[] chars = new char[33];
+                chars = "абвгдеёжзийклмнопрстуфхцчшщъыьэюя".ToCharArray();
+                Chars = chars;
+            }
+            if (language == "Ru") 
+            {
+                char[] chars = new char[33];
+                chars = "АБВГДЕЁЖЗИЙКЛМНОПРСТУФХЦЧШЩЪЫЬЭЮЯ".ToCharArray();
+                Chars = chars;
+            }
+            if (language == "en")
+            {
+                char[] chars = new char[26];
+                chars = "abcdefghijklmnopqrstuvwxyz".ToCharArray();
+                Chars = chars;
+            }
+            if (language == "En")
+            {
+                char[] chars = new char[26];
+                chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ".ToCharArray();
+                Chars = chars;
+            }
+            if (language == "num")
+            {
+                char[] chars = new char[10];
+                chars = "0123456789".ToCharArray();
+                Chars = chars;
+            }
+            if (language == "sp")
+            {
+                char[] chars = new char[50];
+                chars = ".,;:-=+_!&?/".ToCharArray();
+                Chars = chars;
+            }
+        }
+    }
+}
