fecab0eb44e2418a8bec6809707978f6b20d6652
st56 <skadeshopp@gmail.com>
2026-03-10 09:49:51 +0300

    .

---
+++ ChartTest/ChartTest/MainForm.cs
@@ -0,0 +1,110 @@
+﻿using LiveCharts;
+using LiveCharts.Definitions.Series;
+using LiveCharts.Wpf;
+using SalesLibrary;
+using SalesLibrary.Analysis;
+using SalesLibrary.Presenters;
+using SalesLibrary.Views;
+using System;
+using System.Collections.Generic;
+using System.Windows.Forms;
+using System.Windows.Media;
+
+namespace ChartTest
+{
+    public partial class MainForm : Form
+    {
+        private SalesPresenter presenter_;
+        void FillCartesianChart()
+        {
+            ItemsList.DataSource = presenter_.GetAllItems();
+            ItemsList.DisplayMember = "Name";
+            if (ItemsList.Items.Count > 0)
+            {
+                presenter_.ShowSalesByItem(((Item)ItemsList.Items[0]).Name);
+            }
+        }
+
+        void FillAngular()
+        {
+            angular.FromValue = 0;
+            angular.ToValue = 100;
+
+            angular.TicksForeground = Brushes.Gray;
+            angular.NeedleFill = Brushes.DarkBlue;
+        }
+
+        void FillSolid()
+        {
+            solid.From = 0;
+            solid.To = 100;
+            solid.LabelFormatter = value => value + "%";
+        }
+        void FillPie()
+        {
+            List<Item> items = presenter_.GetAllItems();
+
+            double zef = ProfitAnalyzer.CalculateProfitPercentByItem("Зефирка Воздушная", presenter_.GetModel());
+            double candy = ProfitAnalyzer.CalculateProfitPercentByItem("Конфетки Заоблачные", presenter_.GetModel());
+            int temp = Convert.ToInt32(zef * 100);
+            zef = temp / 100;
+            temp = Convert.ToInt32(candy * 100);
+            candy = temp / 100;
+
+            Func<ChartPoint, string> labelPoint = chartPoint => string.Format("{0} ({1:P})", chartPoint.Y, chartPoint.Participation);
+            SeriesCollection piechartData = new SeriesCollection
+            {
+                new PieSeries
+                {
+                    Title = "Зефирки",
+                    Values = new ChartValues<double> { zef },
+                    DataLabels = true,
+                    LabelPoint = labelPoint,
+                    Fill = System.Windows.Media.Brushes.Blue
+                },
+                new PieSeries
+                {
+                    Title = "Конфеты",
+                    Values = new ChartValues<double> { candy },
+                    DataLabels = true,
+                    LabelPoint = labelPoint,
+                    Fill = System.Windows.Media.Brushes.Red
+                }
+            };
+            pie.Dock = DockStyle.Fill;
+            pie.Series.Clear();
+            pie.Series = piechartData;
+            pie.LegendLocation = LegendLocation.Bottom;
+        }
+        public MainForm()
+        {
+            InitializeComponent();
+
+            presenter_ = new SalesPresenter(new List<ISalesView> { cartesian });
+
+            FillCartesianChart();
+
+            FillAngular();
+
+            FillSolid();
+
+            FillPie();
+        }
+
+        private void ItemsList_SelectedIndexChanged(object sender, System.EventArgs e)
+        {
+            Item selectedItem = ((Item)(ItemsList.SelectedItem));
+            if(selectedItem == null)
+            {
+                return;
+            }
+
+            presenter_.ShowSalesByItem(selectedItem.Name);
+            double percent = Math.Round(
+                presenter_.GetProfitPercentByItem(selectedItem), 2);
+
+            angular.Value = percent;
+            solid.Value = percent;
+        }
+    }
+}
