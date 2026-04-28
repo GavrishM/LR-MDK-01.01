23697b68bba8ef55b04ea46ac4e5a1be2c70a55f
Gavrilchenko-Stepan <atom47228@gmail.com>
2026-04-28 10:54:52 +0300

    Код улучшен(визуально)

--- ЛР2/LR2_V7_Gavrish_M/LR2_V7_Gavrish_M/Program.cs
+++ ЛР2/LR2_V7_Gavrish_M/LR2_V7_Gavrish_M/Program.cs
@@ -11,47 +11,61 @@ namespace LR2_V7_Gavrish_M
         static void Main(string[] args)
         {
             Dictionary<string, Dictionary<string, List<double>>> categories = new Dictionary<string, Dictionary<string, List<double>>>();
-            categories.Add("крупы", new Dictionary<string, List<double>>());
-            categories.Add("овощи", new Dictionary<string, List<double>>());
-            categories.Add("фрукты", new Dictionary<string, List<double>>());
-            categories["крупы"].Add("пшено", new List<double>());
-            categories["крупы"].Add("рис", new List<double>());
-            categories["крупы"].Add("овес", new List<double>());
-            categories["овощи"].Add("морковь", new List<double>());
-            categories["овощи"].Add("свекла", new List<double>());
-            categories["овощи"].Add("репа", new List<double>());
-            categories["фрукты"].Add("яблоко", new List<double>());
-            categories["фрукты"].Add("груша", new List<double>());
-            categories["фрукты"].Add("киви", new List<double>());
-            categories["крупы"]["пшено"].Add(1);
-            categories["крупы"]["пшено"].Add(4);
-            categories["крупы"]["пшено"].Add(5);
-            categories["крупы"]["рис"].Add(2);
-            categories["крупы"]["рис"].Add(4);
-            categories["крупы"]["рис"].Add(5);
-            categories["крупы"]["овес"].Add(6);
-            categories["крупы"]["овес"].Add(4);
-            categories["крупы"]["овес"].Add(5);
-            categories["овощи"]["морковь"].Add(1);
-            categories["овощи"]["морковь"].Add(4);
-            categories["овощи"]["морковь"].Add(5);
-            categories["овощи"]["свекла"].Add(2);
-            categories["овощи"]["свекла"].Add(4);
-            categories["овощи"]["свекла"].Add(5);
-            categories["овощи"]["репа"].Add(6);
-            categories["овощи"]["репа"].Add(4);
-            categories["овощи"]["репа"].Add(5);
-            categories["фрукты"]["яблоко"].Add(1);
-            categories["фрукты"]["яблоко"].Add(4);
-            categories["фрукты"]["яблоко"].Add(5);
-            categories["фрукты"]["груша"].Add(2);
-            categories["фрукты"]["груша"].Add(4);
-            categories["фрукты"]["груша"].Add(5);
-            categories["фрукты"]["киви"].Add(6);
-            categories["фрукты"]["киви"].Add(4);
-            categories["фрукты"]["киви"].Add(5);
-            
-            Output.List(Calc.SortList(Calc.FindAveragePrice(Calc.FindProductsList(Input.UserQuery(categories), categories))));
+            //Заполнение
+            {
+                categories.Add("крупы", new Dictionary<string, List<double>>());
+                categories.Add("овощи", new Dictionary<string, List<double>>());
+                categories.Add("фрукты", new Dictionary<string, List<double>>());
+            }
+            {
+                categories["крупы"].Add("пшено", new List<double>());
+                categories["крупы"].Add("рис", new List<double>());
+                categories["крупы"].Add("овес", new List<double>());
+                categories["овощи"].Add("морковь", new List<double>());
+                categories["овощи"].Add("свекла", new List<double>());
+                categories["овощи"].Add("репа", new List<double>());
+                categories["фрукты"].Add("яблоко", new List<double>());
+                categories["фрукты"].Add("груша", new List<double>());
+                categories["фрукты"].Add("киви", new List<double>());
+            }
+            {
+                categories["крупы"]["пшено"].Add(1);
+                categories["крупы"]["пшено"].Add(4);
+                categories["крупы"]["пшено"].Add(5);
+                categories["крупы"]["рис"].Add(2);
+                categories["крупы"]["рис"].Add(4);
+                categories["крупы"]["рис"].Add(5);
+                categories["крупы"]["овес"].Add(6);
+                categories["крупы"]["овес"].Add(4);
+                categories["крупы"]["овес"].Add(5);
+                categories["овощи"]["морковь"].Add(1);
+                categories["овощи"]["морковь"].Add(4);
+                categories["овощи"]["морковь"].Add(5);
+                categories["овощи"]["свекла"].Add(2);
+                categories["овощи"]["свекла"].Add(4);
+                categories["овощи"]["свекла"].Add(5);
+                categories["овощи"]["репа"].Add(6);
+                categories["овощи"]["репа"].Add(4);
+                categories["овощи"]["репа"].Add(5);
+                categories["фрукты"]["яблоко"].Add(1);
+                categories["фрукты"]["яблоко"].Add(4);
+                categories["фрукты"]["яблоко"].Add(5);
+                categories["фрукты"]["груша"].Add(2);
+                categories["фрукты"]["груша"].Add(4);
+                categories["фрукты"]["груша"].Add(5);
+                categories["фрукты"]["киви"].Add(6);
+                categories["фрукты"]["киви"].Add(4);
+                categories["фрукты"]["киви"].Add(5);
+            }
+
+            //Вывод
+            {
+                string userQuery = Input.UserQuery(categories);
+                Dictionary<string, List<double>> findProductList = Calc.FindProductsList(userQuery, categories);
+                Dictionary<string, double> findAveragePrice = Calc.FindAveragePrice(findProductList);
+                Dictionary<string, double> sortList = Calc.SortList(findAveragePrice);
+                Output.List(sortList);
+            }
 
         }
     }
